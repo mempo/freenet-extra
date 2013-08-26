e30a77a518887188775859d5d3a822ba6bffe498
mempo-devel1 <opensource@mempo.org>
2013-08-26 16:13:26 +0200

    A bit more verbose texts

--- oneclick-verify-freenet-version.sh
+++ oneclick-verify-freenet-version.sh
@@ -28,14 +28,15 @@ if [ -z "$config_version" ] ; then
 	script_error
 fi
 
+echo ""
 echo "---------------------------------"
 echo "Will verify Freenet build version $config_version"
 echo "Using directory $config_basedir [PRIVATE]" # private, it might be absolute e.g. with username
 echo ""
 echo "We will use:"
-echo "  * download over clear-internet (your IP is seen by ISP,NSA,etc)"
-echo "  * as well as download from Freenet (you need to have Freenet running)"
-echo ""
+echo "  * download over clear-internet (your IP is seen by ISP and everyone around) !!!"
+echo "  * also download from Freenet (need to have Freenet running)"
+echo "it will be visible to others that you (IP) are verifing freenet (unless e.g. VM on TOR) "
 
 if [ "$2" != "--no-confirmation-start" ] 
 then
@@ -54,14 +55,17 @@ echo "---------------------------------"
 
 if [ -d "$config_basedir" ] ; then
 	echo "The directory $config_basedir exists, probably from previous verification."
-	echo "DELETE THIS DIRECTORY? (y/n)"
+	echo "DELETE THIS DIRECTORY? Write word 'del' if you agree or else to cancel." 
+	echo "Delete? (del/n)"
 	read reply
-	if [ "$reply" != "y" ]
+	if [ "$reply" != "del" ]
 	then
-		echo "You said to NOT continue (not deleting this directoru) - ok aborting (you can delete it manually and restart)"
+		echo "You said to NOT continue (not deleting this directory) - ok aborting (you can delete it manually and restart)"
 		script_error
 	fi
+	echo "Ok, DELETING this directory"
 	rm -rf "$config_basedir" || { echo "Can not delete old dir"; script_error; }
+	echo "Deleted."
 fi
 
 # --- preparation to verify ----
@@ -124,7 +128,22 @@ export PATH=$PATH:/home/fn_verify/.local/bin
 cd ../
 cd scripts/
 
+echo ""
 echo "--- Doing the MAIN VERIFICATION ---"
 
-./verify-build || { echo "Error in verification?"; script_error; }
+./verify-build || { echo "Error in verification?"; echo "-----" ; 
+	echo "*** IF the verification shows the files are DIFFERENT then please warn other users." ; 
+	echo " -----"; script_error; 
+}
+
+set +x
+echo ""
+echo "You should paste results of the verification to other users"
+echo "In Freenet: on FMS, Sone, Frost if you can"
+echo ""
+echo "Please REMEMBER, posting above LOG *will* de-anonymize you unless in TOR'red VM"
+echo "(by times/speeds of downloads, also username etc)"
+echo "But you could edit it manually, or just left the differences > < + - if any."
+echo "Or just write if the build checked out, and paste us the finall checksums."
+echo ""
 
